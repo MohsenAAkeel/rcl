c53bfc0f57f7355778bf734d08ef04d00517d4af
Mido Abu-Akeel <akeelmido@gmail.com>
2023-05-04 22:30:19 -0400

    adding stats

--- ping/src/main.rs
+++ ping/src/main.rs
@@ -1,15 +1,21 @@
 use std::net::{IpAddr, Ipv4Addr};
-use pnet::packet::ip::IpNextHeaderProtocols::Icmp;
+use pnet::packet::{
+    ip::IpNextHeaderProtocols::Icmp,
+    FromPacket,
+    Packet,
+    icmp::{
+        echo_request::MutableEchoRequestPacket, 
+        IcmpPacket
+    }
+};
 use pnet::transport::{TransportProtocol, icmp_packet_iter, transport_channel, TransportChannelType::Layer4};
-use pnet::packet::icmp::{echo_request::MutableEchoRequestPacket, IcmpPacket};
 use std::{thread, time::{Duration, Instant}};
-use pnet::packet::FromPacket;
-
 
 fn main() {
     let ipadd = Ipv4Addr::new(192, 168, 50, 87);
     let _res = ping(IpAddr::V4(ipadd), 1000);
 }
+
 
 /* the `ping` method provides the major functionality of this crate.
  * It sends and receives ICMP packets.
@@ -23,12 +29,18 @@ fn main() {
  *      no return value
  *
  */
-
 fn ping(ip: IpAddr, freq: u64) -> (){
+    let mut requests = 0; // the echo request count 
+    let mut replies = 0; // the recho reply count
+    let mut min: Duration;
+    let mut max: Duration;
+    let mut rtts: Vec<Duration>;
+    ctrlc::set_handler(move || calc_stats(requests, replies, min, max, rtts))
+        .expect("Could not calculate stats");
     let prot = TransportProtocol::Ipv4(Icmp);
     // even though ICMP is a layer 3 (network layer) protocol,
     // pnet needs to open a layer 4 (transport layer) channel
-    let chan = transport_channel(1500, Layer4(prot)); 
+    let chan = transport_channel(64, Layer4(prot)); 
     
     // destructure the channel to access the send and recv structs
     let (mut send, mut recv) = match chan {
@@ -36,32 +48,51 @@ fn ping(ip: IpAddr, freq: u64) -> (){
         Err(e) => panic!("could not create channel: {}", e)
     };
    
-    let mut buf = [0u8; 1500]; // buffer for the Echo Request packet
+    let mut buf = [0u8; 64]; // buffer for the Echo Request packet
 
     // packet iterator that takes receiver struct and returns each
     // echo reply on request
     let mut piter = icmp_packet_iter(&mut recv);
-    println!("running");
+    println!("pinging");
 
-    let timeout = Duration::from_millis(freq); 
-    let mut seq = 1;
-    let mut resp = 0;
+    let timeout = Duration::from_millis(freq);  // the wait time between echo
+                                                // requests
+
     loop {
         let packet = MutableEchoRequestPacket::new(&mut buf).unwrap();
+        //packet.set_payload(&[requests]);
         let sent = send.send_to(packet, ip);
         let now=Instant::now(); 
         match sent {
-            Ok(_) => (),
+            Ok(_) => {
+                requests += 1;
+                ()
+            },
             Err(e) => panic!("Failed to send ICMP packet, {}", e)
         }
 
         match piter.next() {
             Ok((pkt, adr)) => {
-                println!("{} bytes from {}: icmp_seq={} time={:?}", IcmpPacket::packet_size(&(pkt.from_packet())), adr, seq, now.elapsed());
-                seq += 1;
+                println!("{} bytes from {}: icmp_seq={:?} time={:?}", 
+                    IcmpPacket::packet_size(&(pkt.from_packet())), 
+                    adr, 
+                    pkt.payload(), 
+                    now.elapsed()
+                );
+                replies += 1;
             },
             Err(e) => panic!("failed on recv, {}", e)
         }
         thread::sleep(timeout); 
     } 
+}
+
+fn calc_stats(requests: u32, replies: u32, min: Duration, max: Duration, rtts: Vec<Duration>) -> () {
+    let mut rate: f32 = 0.0; 
+    if requests != 0 {
+        rate = 100.0 * replies as f32 / requests as f32;
+    }
+
+    println!("{} packets transmitted, {} packets received, {}% packet loss", requests, replies, rate);
+    println!("rount-trip min/avg/max = {:?}/{:?}/{:?}", min, rtts, max);
 }
